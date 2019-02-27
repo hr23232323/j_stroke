a790a6c9d99cbfe6928ab68474342fc94a068a74
Harsh Rana <hr23232323@gmail.com>
2019-02-27 14:30:40 -0500

    shot curve graph working

--- shot-curve/script.js
+++ shot-curve/script.js
@@ -6,7 +6,7 @@ function makeShortChart(x, y) {
         var margin = {
             top: 20,
             right: 0,
-            bottom: 0,
+            bottom: 20,
             left: 30
         }
 
@@ -16,10 +16,19 @@ function makeShortChart(x, y) {
             .attr('width', width)
             .attr('height', height);
 
+
+
+        var xScale = d3.scaleLinear()
+            .domain([-5, 25])
+            .range([0, width - margin.right - margin.left]);
+        var yScale = d3.scaleLinear()
+            .domain([0, 100])
+            .range([height - margin.top - margin.bottom, 0]);
+
         function getPoint(x) {
             return (-1 * Math.pow(x, 2))
         }
-        //data = data.slice(0, 4000)
+        //data = data.slice(0, 1000)
         data.forEach(function (d, i) {
             var rand = Math.random()
             d.SHOT_DISTANCE = +d.SHOT_DISTANCE;
@@ -31,7 +40,7 @@ function makeShortChart(x, y) {
 
             } else {
                 //2 point
-                x1 = 0 + ((23 - d.SHOT_DISTANCE) / 1.5)
+                x1 = 0 + (23 - d.SHOT_DISTANCE)
             }
 
             x1 -= rand
@@ -41,23 +50,28 @@ function makeShortChart(x, y) {
             if (x1 < 0) {
                 // 3 point
                 x2 = randomNumber(12, 20)
-                y2 = randomNumber(85, 92)
+                y2 = randomNumber(85, 98)
 
             } else if (x1 < 5) {
                 // Long 2
                 x2 = randomNumber(16, 22)
-                y2 = randomNumber(80, 85)
-            } else if (x1 < 10) {
-                // V short shot
+                y2 = randomNumber(75, 85)
+            } else if (x1 < 12) {
+                // short shot
                 x2 = randomNumber(18, 23)
                 y2 = randomNumber(75, 85)
+            } else if (x1 < 16) {
+                // v short shot
+                x2 = randomNumber(20, 24)
+                y2 = randomNumber(72, 78)
+
             } else {
                 // layup
-                x2 = randomNumber(20, 23)
-                y2 = randomNumber(75, 80)
+                x2 = randomNumber(23, 24)
+                y2 = randomNumber(70, 75)
             }
 
-            x3 = 25
+            x3 = randomNumber(24, 25)
             y3 = 69
             var curve = []
             curve.push({
@@ -71,23 +85,39 @@ function makeShortChart(x, y) {
                 y: y3
             })
 
-            redraw(svg, curve, width, height, margin, d.SHOT_MADE_FLAG);
+            redraw(svg, curve, width, height, margin, d.SHOT_MADE_FLAG, xScale, yScale);
         })
+
+
+
+        var x_axis = d3.axisBottom()
+            .scale(xScale)
+            .ticks(30, "f")
+            .tickSize(0)
+            .tickFormat(function (d) {
+                console.log(d)
+                d = -d + 23
+                var mapper = {
+                    23: "~23 ft.",
+                    14: "~14 ft.",
+                    4: "4 ft.",
+                    0: "Under Rim"
+                }
+
+                return mapper[d]
+            })
+
+        svg.append("g")
+            .attr("transform", "translate(0," + (height - 20) + ")")
+            .call(x_axis);
     })
 }
 
-function redraw(svg, curve, width, height, margin, made) {
+function redraw(svg, curve, width, height, margin, made, xScale, yScale) {
     var interpolation = "d3.curveMonotoneX";
     var graphGroup = svg.append('g')
         .attr('transform', 'translate(' + margin.left + ', ' +
             margin.top + ')');
-
-    var xScale = d3.scaleLinear()
-        .domain([-15, 25])
-        .range([0, width - margin.right - margin.left]);
-    var yScale = d3.scaleLinear()
-        .domain([0, 100])
-        .range([height - margin.top - margin.bottom, 0]);
 
     graphGroup.selectAll('path').remove();
 
@@ -105,9 +135,9 @@ function redraw(svg, curve, width, height, margin, made) {
         .attrs({
             d: line,
             stroke: 'steelBlue',
-            'stroke-width': 0.1,
+            'stroke-width': 0.2,
             fill: 'none',
-            opacity: 0.1
+            opacity: 0.075
         });
 }
 
